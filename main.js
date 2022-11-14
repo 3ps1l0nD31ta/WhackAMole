83b65ddc671f3033be6aab08e134b6f5572ea8c3
3ps1l0nD31ta <lukepearson115@gmail.com>
2022-11-14 19:26:58 +0000

    Canvas set up with button press

---
+++ main.js
@@ -0,0 +1,67 @@
+function Vector2(x,y){
+    this.x = x;
+    this.y = y;
+}
+function vecDelta(v1,v2)
+{
+    returnVec = new Vector2(v1.x-v2.x,v1.y-v2.y);
+    return(returnVec);
+}
+function magnitude(vec)
+{
+    return(Math.sqrt(vec.x*vec.x+vec.y*vec.y));
+}
+function Target(x,y,radius)
+{
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+}
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  vec = new Vector2();
+  return {x:evt.clientX - rect.left,y:evt.clientY - rect.top};
+}
+function drawCircle(x,y,colour)
+{
+    ctx.fillStyle = colour;
+    ctx.beginPath();
+    ctx.arc(x, y, 15, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "#003300";
+    ctx.stroke();
+}
+function draw(evt) {
+  var pos = getMousePos(gameWindow, evt);
+  drawCircle(pos.x,pos.y,"#005500");
+}
+
+function withinTarget(pos,targ, radius)
+{
+    if(magnitude(vecDelta(pos,targ))<radius)
+    {
+        return(true);
+    }
+    return(false);
+}
+
+function onMouseDown(evt)
+{
+    var mousePos = getMousePos(gameWindow,evt);
+    if(withinTarget(mousePos,{x: target.x,y: target.y},target.radius))
+    {
+        console.log("hit button");
+    }
+}
+
+//
+
+//init game window
+var gameWindow = document.getElementById("gameWindow");
+var ctx = gameWindow.getContext("2d");
+//add draw to mousedown event
+gameWindow.addEventListener("mousedown",function(evt){onMouseDown(evt);});
+//drawStartButton
+drawCircle(gameWindow.clientWidth/2,gameWindow.clientHeight/2,"#005500");
+var target = new Target(gameWindow.clientWidth/2,gameWindow.clientHeight/2,15);
