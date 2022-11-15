46eaffa0b312864cab95e6a03b9798c91abc2747
3ps1l0nD31ta <lukepearson115@gmail.com>
2022-11-15 20:54:55 +0000

    button press game loop

--- main.js
+++ main.js
@@ -1,58 +1,63 @@
-class Vector2
-{
-    constructor(x,y){
-        this.x = x;
-        this.y = y;
-    }
-
-    magnitude() {
-        return Math.sqrt(this.x*this.x+this.y*this.y);
-    }
-    static vectorAdd(v1,v2)
-    {
-        return new Vector2(v1.x+v2.x,v1.y+v2.y);
-    }
-    static VectorMult(vec,scalar)
-    {
-        var returnVec = new Vector2(vec.x,vec.y);
-        returnVec.x *= scalar;
-        returnVec.y *= scalar;
-        return returnVec;
-    }
+function Vector2(x,y){
+    this.x = x;
+    this.y = y;
 }
-class Target extends Vector2{
-    constructor(x,y,radius)
-    {
-        super(x,y);
-        this.radius = radius;
-    }
+function vecDelta(v1,v2)
+{
+    returnVec = new Vector2(v1.x-v2.x,v1.y-v2.y);
+    return(returnVec);
+}
+function magnitude(vec)
+{
+    return(Math.sqrt(vec.x*vec.x+vec.y*vec.y));
+}
+function Target(x,y,radius)
+{
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
 }
 function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
-  return new Vector2(evt.clientX - rect.left,evt.clientY - rect.top);
+  vec = new Vector2();
+  return {x:evt.clientX - rect.left,y:evt.clientY - rect.top};
 }
-function drawCircle(vec,colour)
+function drawCircle(x,y,colour)
 {
     ctx.fillStyle = colour;
     ctx.beginPath();
-    ctx.arc(vec.x, vec.y, 15, 0, 2 * Math.PI);
+    ctx.arc(x, y, 15, 0, 2 * Math.PI);
     ctx.fill();
     ctx.lineWidth = 5;
     ctx.strokeStyle = "#003300";
     ctx.stroke();
 }
-function withinTarget(pos,targ)
-{   
-    return (Vector2.vectorAdd(pos,Vector2.VectorMult(targ,-1))).magnitude()<targ.radius;
+function draw(evt) {
+  var pos = getMousePos(gameWindow, evt);
+  drawCircle(pos.x,pos.y,"#005500");
 }
+
+function withinTarget(pos,targ, radius)
+{
+    if(magnitude(vecDelta(pos,targ))<radius)
+    {
+        return(true);
+    }
+    return(false);
+}
+
 function onMouseDown(evt)
 {
     var mousePos = getMousePos(gameWindow,evt);
-    if(withinTarget(mousePos,target))
+    if(withinTarget(mousePos,{x: target.x,y: target.y},target.radius))
     {
         console.log("hit button");
+        hitStart = !hitStart;
     }
 }
+
+//
+
 //init game window
 var gameWindow = document.getElementById("gameWindow");
 var ctx = gameWindow.getContext("2d");
@@ -60,7 +65,51 @@ var ctx = gameWindow.getContext("2d");
 gameWindow.addEventListener("mousedown",function(evt){onMouseDown(evt);});
 //init variables
 let centre = new Vector2(gameWindow.clientWidth/2,gameWindow.clientHeight/2);
-var target = new Target(centre.x,centre.y,15);
+var target = new Target(centre.x,centre.y,20);
+var hitStart = false;
+var counting = false;
 //drawStartButton
 drawCircle(centre,"#005500");
+
+function update(progress)
+{
+    if(hitStart)
+    {
+        counting = true;
+    }
+    if(counting)
+    {
+        count += progress;
+    }
+    if(count > 2000)
+    {
+        drawCircle(centre,"#005500");
+        counting = false;
+        hitStart = false;
+        count = 0;
+    }
+    console.log(counting);
+}
+
+function draw()
+{
+    if(hitStart)
+    {
+        ctx.clearRect(centre.x-target.radius,centre.y-target.radius,target.radius*2,target.radius*2);
+    }
+}
+
+function loop(timestamp)
+{
+    var progress = timestamp - lastRender;
+
+    update(progress);
+    draw();
+
+    lastRender = timestamp;
+    window.requestAnimationFrame(loop);
+}
+var lastRender = 0;
+var count = 0;
+window.requestAnimationFrame(loop);
 
